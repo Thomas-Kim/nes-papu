e2ffc19d371a923593980a26412b6f62448b0abd
Thomas Kim <thomask@cs.utexas.edu>
2015-05-11 17:38:41 -0500

    Modified audio_effects.v to test new clock

--- audio_effects.v
+++ audio_effects.v
@@ -269,10 +269,12 @@ initial begin
 
 end
 
+reg[6:0] sqc = 0;
 always @(posedge clk) begin
     if (sample_end) begin
         last_sample <= audio_input;
     end
+    sqc <= sqc + 1;
 
     /*if (sample_req) begin
         if (control[FEEDBACK])
@@ -295,6 +297,12 @@ always @(posedge clk) begin
 	 else if(control[1]) begin
 		dat <= sq1_out > 0 ?  16'h4000 : 0;
 	 end
+         else if(control[2]) begin
+                dat <= noise_out > 0 ? 16'h4000 : 0;
+         end
+         else if(control[3]) begin
+                dat <= sqc > 7'b1000000 ? 16'h4000 : 0;
+         end
 end
 
 endmodule
